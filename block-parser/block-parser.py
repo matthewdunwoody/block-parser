0601ebf2f02e087032bad1ba62f1540afac735b7
@matthewdunwoody <mr.m.dunwoody@gmail.com>
2016-03-09 21:46:33 -0500

    Update block-parser.py

--- block-parser/block-parser.py
+++ block-parser/block-parser.py
@@ -153,7 +153,7 @@ def output_result(blocks, metadata, o, f, m):
 def main():
     import argparse
     parser = argparse.ArgumentParser(
-        description="Parse PowerShell script block log entries (EID 4104) out of the Microsoft-Windows-PowerShell%%4Operational.evtx event log. By default, reconstructs all multi-message blocks.")
+        description="Parse PowerShell script block log entries (EID 4104) out of the Microsoft-Windows-PowerShell%4Operational.evtx event log. By default, reconstructs all multi-message blocks.")
     parser.add_argument("evtx", type=str,
                         help="Path to the Microsoft-Windows-PowerShell%%4Operational.evtx event log file to parse")
     parser.add_argument("-m",  "--metadata", type=str,
